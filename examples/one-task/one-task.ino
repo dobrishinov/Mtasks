631beffa9b7d8c7a731078f799355c9246f11162
dobrishinov <dobrishinov@gmail.com>
2019-10-17 14:36:36 +0300

    [LOGIC] Added example sketch: one-task.ino

---
+++ examples/one-task/one-task.ino
@@ -0,0 +1,13 @@
+#include <Mtasks.h>
+
+Mtasks taskOne(1000); //Create a task and specify the duration in miliseconds
+
+void setup(){
+  Serial.begin(115200);
+}
+
+void loop(){
+  if(taskOne.isTaskReady()){ //Check if task is ready
+    Serial.println("I'm Ready!"); //Call logic for your needs
+  }
+}
